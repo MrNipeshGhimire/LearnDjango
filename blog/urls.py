8fea1f1b668c32710aa8861a78c39a466c278bd3
MrNipeshGhimire <nipeshincbca2021@gmail.com>
2024-12-07 12:46:22 +0545

    vjch

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views.main_view import home
+from .views.main_view import home,single_blog,edit_blog,create_blog
+from .views.auth_view import register,login
 
 urlpatterns = [
- path("/",home)
+ path("",home),
+ path("register/",register),
+ path("login/",login),
+ path("create/",create_blog),
+ path("edit/",edit_blog),
+ path("single/",single_blog)
 ]
