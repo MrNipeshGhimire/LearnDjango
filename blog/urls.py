2ee9f3fc6f2af71dd11292c1fcfb2e2a32236462
MrNipeshGhimire <nipeshincbca2021@gmail.com>
2024-12-12 12:50:13 +0545

    CREATE BLOG

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from .views.main_view import home,single_blog,edit_blog,create_blog
 from .views.auth_view import register,login
 
 urlpatterns = [
- path("",home),
- path("register/",register),
+ path("",home, name="home"),
+ path("register/",register, name="register"),
  path("login/",login),
  path("create/",create_blog),
  path("edit/",edit_blog),
--- blog/views/main_view.py
+++ blog/views/main_view.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from ..models import Blog
 
 def home(request):
     return render(request,'main/home.html')
 
 def create_blog(request):
+
+    if request.method == "POST":
+        title = request.POST.get("title")
+        subtitle = request.POST.get("subtitle")
+        description = request.POST.get("description")
+        blog = Blog(title=title,subtitle=subtitle,description=description)
+        blog.save()
+        return redirect("home")
+
     return render(request,'main/create_blog.html')
 
 def single_blog(request):
